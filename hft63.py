8708bc9fa8311e6019b5e68ba86d07a9d290085c
xmd79 <34522857+xmd79@users.noreply.github.com>
2023-07-08 02:16:45 +0300

    Update hft63.py

--- hft63.py
+++ hft63.py
@@ -1,3 +1,16 @@
+import math
+import time
+import numpy as np
+import requests
+import talib
+import json
+import datetime
+from datetime import timedelta
+from decimal import Decimal
+import decimal
+import random
+import statistics
+
 # binance module imports
 from binance.client import Client as BinanceClient  
 from binance.exceptions import BinanceAPIException, BinanceOrderException
@@ -61,7 +74,7 @@ def get_30m_candles(symbol):
 def get_1h_candles(symbol):   
     return get_candles(symbol, client.KLINE_INTERVAL_1HOUR)
 
-candles = get_1m_candles("BTCUSDT")
+candles = get_5m_candles("BTCUSDT")
 print(candles)
 
 # Get the last price of a given symbol pair from Binance    
@@ -71,7 +84,7 @@ def get_current_price(symbol):
     return float(ticker['price'])
 
 price = get_current_price("BTCUSDT")
-print(price)
+#print(price)
 
 period = 200
 
@@ -86,3 +99,87 @@ def calculate_ema(candles, period):
     return ema
 
 calculate_ema(candles, period)
+
+price_level = 100
+sine_indicator = 0.5
+
+VOLATILITY = 0.05 # 5% volatility
+
+# Define PHI constant with 15 decimals
+PHI = 1.6180339887498948482045868343656381177
+
+def golden_func(n):
+    if isinstance(n,np.ndarray):
+        result= PHI**n[:,np.newaxis] 
+    else: result= PHI**n     
+    result=result.round(15)        
+    return result
+
+def calculate_sine(data): 
+    data = np.array([[c['open'], c['high'], c['low'],  
+                      c['close'], c['volume']] for c in candles])   
+    norm_sine=(data[:, 3]-np.min(data[:, 3]))/(np.max(data[:, 3])-np.min(data[:, 3]))       
+    return norm_sine      
+
+def calculate_reversal(price_level,norm_sine):      
+    try:     
+        if norm_sine<0 or norm_sine>1: raise ValueError  
+    except ValueError:         
+        return 0         
+    result= PHI**norm_sine         
+    result=result.round(15)       
+    reversal_point=result*price_level       
+    return reversal_point
+
+def get_reversal_points(candles):       
+    if len(candles)==0:
+        return 0         
+    close=candles[-1]['close']       
+    data=np.array([[c['open'],c['high'],c['low'],c['close'],c['volume']]for c in candles])        
+    norm_sine=calculate_sine(data)        
+    high=np.max(data[:,1])         
+    low=np.min(data[:,2])             
+    quad_1_high=high*1.618   
+    quad_2_low=high   
+    quad_3_high=low/1.618
+    quad_4_low=low 
+    reversal= 0                  
+    if close>quad_1_high:current_quadrant=2        
+    elif close<quad_3_high:current_quadrant=4            
+    else : current_quadrant=1 if close>high/2 else 3     
+    if current_quadrant==2:                               
+        reversal=quad_2_low*calculate_reversal(quad_2_low,norm_sine)         
+    return reversal
+    print(current_quadrant)
+    print(norm_sine)
+
+reversal = get_reversal_points(candles)
+print(reversal)
+
+# Get the next 3 reversal points and prices
+def get_next_reversals(candles):
+    reversals = []
+    for i in range(3):
+        candles.append({
+            "open": candles[-1]['close'],
+            "high": candles[-1]['close'],  
+            "low": candles[-1]['close'],
+            "close": candles[-1]['close'],   
+            "volume": 1   
+        })
+        # Calculate reversal point                
+        reversal = get_reversal_points(candles)  
+        
+        # Append result              
+        reversals.append({
+            "reversal": reversal,  
+            "price": candles[-1]['close']       
+        })
+        
+        # Remove the appended candle
+        candles = candles[:-1]
+        
+    return reversals
+
+reversals = get_next_reversals(candles)
+print(reversals)
