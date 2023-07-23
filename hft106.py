2cdde5de5c007ce6e7423b3f0f9da961c03d7361
xmd79 <34522857+xmd79@users.noreply.github.com>
2023-07-23 22:08:36 +0300

    Add files via upload

---
+++ hft106.py
@@ -0,0 +1,146 @@
+##################################################
+##################################################
+
+# Start code:
+
+print()
+
+##################################################
+##################################################
+
+print("Init code: ")
+print()
+
+##################################################
+##################################################
+
+print("Test")
+print()
+
+##################################################
+##################################################
+
+# Import modules:
+
+import math
+import time
+import numpy as np
+import requests
+import talib
+import json
+import datetime
+from datetime import timedelta
+from decimal import Decimal
+import decimal
+import random
+import statistics
+from statistics import mean
+import scipy.fftpack as fftpack
+
+##################################################
+##################################################
+
+# binance module imports
+from binance.client import Client as BinanceClient
+from binance.exceptions import BinanceAPIException, BinanceOrderException
+from binance.enums import *
+
+##################################################
+##################################################
+
+# Load credentials from file
+with open("credentials.txt", "r") as f:
+    lines = f.readlines()
+    api_key = lines[0].strip()
+    api_secret = lines[1].strip()
+
+# Instantiate Binance client
+client = BinanceClient(api_key, api_secret)
+
+##################################################
+##################################################
+
+# Define a function to get the account balance in BUSD
+
+def get_account_balance():
+    accounts = client.futures_account_balance()
+    for account in accounts:
+        if account['asset'] == 'USDT':
+            bUSD_balance = float(account['balance'])
+            return bUSD_balance
+
+# Get the USDT balance of the futures account
+bUSD_balance = float(get_account_balance())
+
+# Print account balance
+print("USDT Futures balance:", bUSD_balance)
+print()
+
+##################################################
+##################################################
+
+# Define Binance client reading api key and secret from local file:
+
+def get_client():
+    # Read credentials from file    
+    with open("credentials.txt", "r") as f:   
+         lines = f.readlines()
+         api_key = lines[0].strip()  
+         api_secret = lines[1].strip()  
+          
+    # Instantiate client        
+    client = BinanceClient(api_key, api_secret)
+          
+    return client
+
+# Call the function to get the client  
+client = get_client()
+
+##################################################
+##################################################
+
+# Initialize variables for tracking trade state:
+
+TRADE_SYMBOL = "BTCUSDT"
+
+##################################################
+##################################################
+
+# Define timeframes and get candles:
+
+timeframes = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h',  '6h', '8h', '12h', '1d']
+
+def get_price(symbol):
+    try:
+        client = get_client()
+        url = "https://fapi.binance.com/fapi/v1/ticker/price"
+        params = {
+            "symbol": symbol 
+            }
+        response = requests.get(url, params=params)
+        data = response.json()
+        if "price" in data:
+            price = float(data["price"])
+        else:
+            raise KeyError("price key not found in API response")
+        return price      
+    except (BinanceAPIException, KeyError) as e:
+        print(f"Error fetching price for {symbol}: {e}")
+        return 0
+
+price = get_price("BTCUSDT")
+
+print(price)
+
+##################################################
+##################################################
+
+
+
+
+
+
+
+
+
+
