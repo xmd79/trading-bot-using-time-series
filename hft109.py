d154476b01e59f001fec369d105eb19cdfbf645d
xmd79 <34522857+xmd79@users.noreply.github.com>
2023-07-26 02:18:46 +0300

    Update hft109.py

--- hft109.py
+++ hft109.py
@@ -1079,20 +1079,28 @@ def main():
             with open("signals.txt", "a") as f:   
                 # Get data and calculate indicators here...
                 timestamp = current_time.strftime("%d %H %M %S")
+
+                pct_diff = None
+
+                if price <= min_threshold:
+                    if momentum > 0:
+                        trigger_long = True
+
+                elif price >= max_threshold:
+                    if momentum < 0:
+                        trigger_short = True
+
                 if current_quadrant == 1: 
 
                     # Add percentage difference condition           
                     pct_diff = (min_threshold - price) / min_threshold * 100
 
                     if pct_diff <= 10:
-
                         if dist_from_close_to_min <= 15:
                             if momentum > 0:
                                 if price < avg_mtf and price < fastest_target and price < target1 and market_mood == "Bullish":
                                     trigger_long = True
 
-                        if price <= min_threshold:
-                            trigger_long = True
 
                 elif current_quadrant == 4: 
 
@@ -1100,14 +1108,10 @@ def main():
                     pct_diff = (price - max_threshold) / max_threshold * 100 
 
                     if pct_diff <= 10:
-
                         if dist_from_close_to_max <= 15:
-                            if momentum < 0:
-                                if price > avg_mtf and price > fastest_target and price > target1 and market_mood == "Bearish":
-                                    trigger_short = True  
-             
-                    if price >= max_threshold:
-                        trigger_short = True
+                        if momentum < 0:
+                            if price > avg_mtf and price > fastest_target and price > target1 and market_mood == "Bearish":
+                                trigger_short = True  
 
                 if trigger_long:          
                     print("LONG signal!")  
@@ -1145,4 +1149,3 @@ print()
 # Run the main function
 if __name__ == '__main__':
     main()
-
