292b73da671e2c4f9a467be2a26f0d0987baf31f
xmd79 <34522857+xmd79@users.noreply.github.com>
2023-08-10 07:38:58 +0300

    Update hft111.py

--- hft111.py
+++ hft111.py
@@ -2922,7 +2922,7 @@ def main():
                                         trigger_long = True
 
                 if current_quadrant == 4:
-                    if price > avg_mtf and price > fastest_target and price > target1 and price > target2 and price > target3 and price > target4 and price > target5 and price > incoming_reversal_keypoint and price > future_price_regression and price > forecast_price_fft and price >= lower_bb_5min[-1]:
+                    if price > avg_mtf and price > fastest_target and price > target1 and price > target2 and price > target3 and price > target4 and price > target5 and price > incoming_reversal_keypoint and price > future_price_regression and price > forecast_price_fft and price >= upper_bb_5min[-1]:
                         if market_mood_sr == "Bearish" or market_mood_sr == "Neutral": 
                             if pct_diff_to_max < pct_diff_to_min and closest_threshold == max_threshold:
                                 if sell_volume_5min > buy_volume_5min:
