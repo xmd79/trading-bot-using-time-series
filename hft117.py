d1be6f7ce849ebe454b33abc383a9970de0eddbe
xmd79 <34522857+xmd79@users.noreply.github.com>
2023-08-20 10:21:16 +0300

    Update hft117.py

--- hft117.py
+++ hft117.py
@@ -3346,16 +3346,16 @@ def main():
                     print("Now not in a trade, seeking entry conditions")
 
                     if current_quadrant == 1 and price < forecast_price_fft:
-                        if market_mood_sr == "Bullish" or market_mood_sr == "Neutral":
-                            if forecast_direction == "Up" and price < avg_mtf and price < fastest_target:
+                        if market_mood_sr == "Bullish" or market_mood_sr == "Neutral" and closest_threshold == min_threshold:
+                            if forecast_direction == "Up" and price < avg_mtf:
                                 if momentum > 0 and buy_volume_1min > sell_volume_1min:
                                     for i, target in enumerate(inner_targets, start=1):
                                         if price < target:
                                             trigger_long = True
 
                     if current_quadrant == 4 and price > forecast_price_fft:
-                        if market_mood_sr == "Bearish" or market_mood_sr == "Neutral":
-                            if forecast_direction == "Down" and price > avg_mtf and price > fastest_target:
+                        if market_mood_sr == "Bearish" or market_mood_sr == "Neutral" and closest_threshold == max_threshold:
+                            if forecast_direction == "Down" and price > avg_mtf:
                                 if momentum < 0 and sell_volume_1min > buy_volume_1min:
                                     for i, target in enumerate(inner_targets, start=1):
                                         if price > target:
