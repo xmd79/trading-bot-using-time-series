f4d2e5058d825b0546aa46c5a835857cd2fb8655
xmd79 <34522857+xmd79@users.noreply.github.com>
2023-09-10 18:11:10 +0300

    Update hft124.py

--- hft124.py
+++ hft124.py
@@ -3478,7 +3478,7 @@ def main():
                     print("Now not in a trade, seeking entry conditions")
 
                     if current_quadrant == 1 and price < fastest_target and normalized_distance_to_min < normalized_distance_to_max:
-                        if market_mood_sr == "Bullish" or market_mood_sr == "Neutral" and closest_threshold == min_threshold:
+                        if market_mood_sr == "Bullish" or market_mood_sr == "Neutral":
                             if market_mood_sine == "Uptrend" and price < avg_mtf and price < middle:
                                 if momentum > 0:
                                     for timeframe in timeframes:
@@ -3488,7 +3488,7 @@ def main():
                                                     trigger_long = True
 
                     if current_quadrant == 4 and price > fastest_target and normalized_distance_to_min > normalized_distance_to_max:
-                        if market_mood_sr == "Bearish" or market_mood_sr == "Neutral" and closest_threshold == max_threshold:
+                        if market_mood_sr == "Bearish" or market_mood_sr == "Neutral":
                             if market_mood_sine == "Downtrend" and price > avg_mtf and price > middle:
                                 if momentum < 0:
                                     for timeframe in timeframes:
