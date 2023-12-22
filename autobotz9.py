c914fe8a7f7f978563a31c71bd0c828c007a5433
xmd79 <34522857+xmd79@users.noreply.github.com>
2023-12-22 06:28:04 +0200

    Update autobotz9.py

--- autobotz9.py
+++ autobotz9.py
@@ -1396,7 +1396,7 @@ def main():
                         print("LONG condition 1: closest_threshold < price")                
                         if price < fastest_target:
                             print("LONG condition 2: price < fastest_target") 
-                            if future_price_regression < price:
+                            if future_price_regression > price:
                                 print("LONG condition 3: future_price_regression < price")
                                 if market_mood_fft == "Bullish":
                                     print("LONG condition 4:market_mood_fft == Bullish")
@@ -1409,7 +1409,7 @@ def main():
                         print("SHORT condition 1: closest_threshold > price")
                         if price > fastest_target:
                             print("SHORT condition 2: price > fastest_target") 
-                            if future_price_regression > price:
+                            if future_price_regression < price:
                                 print("SHORT condition 3: future_price_regression > price")
                                 if market_mood_fft == "Bearish":
                                     print("SHORT condition 4:market_mood_fft == Bearish")
