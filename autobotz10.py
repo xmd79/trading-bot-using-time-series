764871708a3c22e5b89272b991e63e1a7deee1d6
xmd79 <34522857+xmd79@users.noreply.github.com>
2023-12-31 16:45:26 +0200

    Update autobotz10.py

--- autobotz10.py
+++ autobotz10.py
@@ -1359,7 +1359,7 @@ def main():
             ##################################################
 
             take_profit = 5.00
-            stop_loss = -10.00
+            stop_loss = -50.00
 
             # Current timestamp in milliseconds
             timestamp = int(time.time() * 1000)
