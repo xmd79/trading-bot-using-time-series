947222369d6a66479b64be3065aa395d5a7cf67a
xmd79 <34522857+xmd79@users.noreply.github.com>
2024-01-28 06:30:56 +0200

    Update hftautox4.py

--- hftautox4.py
+++ hftautox4.py
@@ -3004,7 +3004,7 @@ def main():
             ##################################################
 
             take_profit = 5.00
-            stop_loss = -25.00
+            stop_loss = -1.00
 
             # Current timestamp in milliseconds
             timestamp = int(time.time() * 1000)
