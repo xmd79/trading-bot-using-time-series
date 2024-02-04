e8e4ae4c37f40fbc2241510ddcd103e74c921bd2
xmd79 <34522857+xmd79@users.noreply.github.com>
2024-02-04 21:33:41 +0200

    Update hftautox8.py

--- hftautox8.py
+++ hftautox8.py
@@ -2735,10 +2735,10 @@ def main():
             print(f"Expected price on the 45-degree angle trend: {expected_price}")
 
             # Generate forecast based on the 45-degree angle
-            #forecast_result = forecast_45_degree_angle(price, expected_price)
+            forecast_result = forecast_45_degree_angle(price, expected_price)
 
             # Display the forecast result
-            #print(forecast_result)
+            print(forecast_result)
 
             print()
 
@@ -3532,19 +3532,19 @@ def main():
                     ##################################################
                     ##################################################
 
-                    if price < forecast_sine_price and normalized_distance_to_min < normalized_distance_to_max and closest_threshold == min_threshold and forecast_direction == "Up" and market_mood_fft == "Bullish" and signal == "BUY" and market_mood_type == "up" and price < forecast and price < expected_price and pivot_mood == "Bullish" and momentum > 0 and positive_count > negative_count and long_conditions_met > short_conditions_met:
+                    if price < forecast_sine_price and normalized_distance_to_min < normalized_distance_to_max and closest_threshold == min_threshold and forecast_direction == "Up" and market_mood_fft == "Bullish" and price < fastest_target and signal == "BUY" and market_mood_type == "up" and price < forecast and price < expected_price and pivot_mood == "Bullish" and momentum > 0 and positive_count > negative_count and long_conditions_met > short_conditions_met:
                         print("HFT LONG signal triggered!") 
                         trigger_long = True
 
-                    elif price > forecast_sine_price and normalized_distance_to_min > normalized_distance_to_max and closest_threshold == max_threshold and forecast_direction == "Down" and  market_mood_fft == "Bearish" and signal == "SELL" and market_mood_type == "down" and price > forecast and price > expected_price and pivot_mood == "Bearish" and momentum < 0 and positive_count < negative_count and long_conditions_met < short_conditions_met:
+                    elif price > forecast_sine_price and normalized_distance_to_min > normalized_distance_to_max and closest_threshold == max_threshold and forecast_direction == "Down" and  market_mood_fft == "Bearish" and price > fastest_target and signal == "SELL" and market_mood_type == "down" and price > forecast and price > expected_price and pivot_mood == "Bearish" and momentum < 0 and positive_count < negative_count and long_conditions_met < short_conditions_met:
                         print("HFT SHORT signal triggered!")  
                         trigger_short = True
 
-                    if price < forecast_sine_price and normalized_distance_to_min < normalized_distance_to_max and closest_threshold == min_threshold and forecast_direction == "Up" and market_mood_fft == "Bullish" and signal == "BUY" and market_mood_type == "up" and price < forecast and price < expected_price and pivot_mood == "Bullish" and momentum > 0 and positive_count == negative_count and long_conditions_met > short_conditions_met:
+                    if price < forecast_sine_price and normalized_distance_to_min < normalized_distance_to_max and closest_threshold == min_threshold and forecast_direction == "Up" and market_mood_fft == "Bullish" and price < fastest_target  and signal == "BUY" and market_mood_type == "up" and price < forecast and price < expected_price and pivot_mood == "Bullish" and momentum > 0 and positive_count == negative_count and long_conditions_met > short_conditions_met:
                         print("HFT LONG signal triggered!") 
                         trigger_long = True
 
-                    elif price > forecast_sine_price and normalized_distance_to_min > normalized_distance_to_max and closest_threshold == max_threshold and forecast_direction == "Down" and  market_mood_fft == "Bearish" and signal == "SELL" and market_mood_type == "down" and price > forecast and price > expected_price and pivot_mood == "Bearish" and momentum < 0 and positive_count == negative_count and long_conditions_met < short_conditions_met:
+                    elif price > forecast_sine_price and normalized_distance_to_min > normalized_distance_to_max and closest_threshold == max_threshold and forecast_direction == "Down" and  market_mood_fft == "Bearish" and price > fastest_target  and signal == "SELL" and market_mood_type == "down" and price > forecast and price > expected_price and pivot_mood == "Bearish" and momentum < 0 and positive_count == negative_count and long_conditions_met < short_conditions_met:
                         print("HFT SHORT signal triggered!")  
                         trigger_short = True
 
