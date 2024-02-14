f4393742470dced9e47746ccc11960cf80b539d2
xmd79 <34522857+xmd79@users.noreply.github.com>
2024-02-14 20:09:58 +0200

    Update hftautox8.py

--- hftautox8.py
+++ hftautox8.py
@@ -3611,8 +3611,8 @@ def main():
             ##################################################
             ##################################################
 
-            take_profit = 3.33
-            stop_loss = -6.66
+            take_profit = 3.66
+            stop_loss = -6.33
 
             # Current timestamp in milliseconds
             timestamp = int(time.time() * 1000)
