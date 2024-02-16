c70f70d71c028bcd2da41ae9eef7f638dc7c3476
xmd79 <34522857+xmd79@users.noreply.github.com>
2024-02-16 15:51:08 +0200

    Update hftautox9.py

--- hftautox9.py
+++ hftautox9.py
@@ -4110,7 +4110,7 @@ def main():
                         print("LONG ultra HFT momentum triggered")
                         trigger_long = True
 
-                    if momentum < 0 and buy_volume_1min < sell_volume_1min and buy_volume_5min < sell_volume_5min and price > hft_target and positive_count < negative_count and signal == "SELL" and market_mood_type == "down" and incoming_reversal == "Dip":
+                    if momentum < 0 and buy_volume_1min < sell_volume_1min and buy_volume_5min < sell_volume_5min and price < hft_target and positive_count < negative_count and signal == "SELL" and market_mood_type == "down" and incoming_reversal == "Dip":
                         print("SHORT ultra HFT momentum triggered")
                         trigger_short = True
 
